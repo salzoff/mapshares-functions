5fd57af6040b93b84d08510b06bec3568d0d93e6
salzoff <salzoff@gmail.com>
2019-03-31 21:36:05 +0200

    latest

--- functions/index.js
+++ functions/index.js
@@ -33,6 +33,31 @@ exports.addBoxCollectionInGeoCollection = functions.firestore
         });
     });
 
+exports.markBoxAsFound = functions.https.onCall((data, context) => {
+    return new Promise(resolve => {
+        return firestore.collection('userProfile').doc(context.auth.uid).get().then(userProfile => {
+            return firestore.collection('box').doc(data.id).update({
+                foundBy: userProfile.ref,
+                foundAt: new Date()
+            }).then(() => {
+                return geoCollection.doc(data.id).update({ objectType: 4 }).then(() => {
+                    return firestore.collection('box').doc(data.id).collection('hints').get().then(hints => {
+                        hints.forEach(hint => {
+                            geoCollection.doc(hint.id + 'hint').delete();
+                        });
+                        resolve({ success: true });
+                        return;
+                    });
+                });
+            }).catch(e => {
+                console.error(e);
+            });
+        });
+    }).catch(e => {
+        console.error(e);
+    });
+});
+
 /* exports.addHintInGeoCollection = functions.firestore
     .document('/box/{bid}/hints/{uid}').onCreate((snap, context) => {
         const data = snap.data();
